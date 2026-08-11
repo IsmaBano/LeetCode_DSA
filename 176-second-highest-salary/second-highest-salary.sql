ad10ad186629d3b00575484ed53ab45f5f1c9665
IsmaBano <155432858+IsmaBano@users.noreply.github.com>
2026-08-11 18:24:13 +0530

    Time: 250 ms (98.47%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,6 +1,11 @@
 # method 1
-select max(salary) as SecondHighestSalary from
-Employee where salary<
-(select max(salary) from Employee)
+-- select max(salary) as SecondHighestSalary from
+-- Employee where salary<
+-- (select max(salary) from Employee)
+
+# method 2
+select coalesce((select distinct salary  from Employee
+order by salary desc
+limit 1 offset 1),null) as SecondHighestSalary
 
  
