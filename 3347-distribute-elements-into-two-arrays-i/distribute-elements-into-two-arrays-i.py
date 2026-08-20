1c20aa50a9586eb849b88ecec95ed30f935eab23
IsmaBano <155432858+IsmaBano@users.noreply.github.com>
2026-08-20 10:50:59 +0530

    Time: 4 ms (6.73%) | Memory: 19.5 MB (2.56%) - LeetSync

--- 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.py
+++ 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.py
@@ -1,21 +1,18 @@
-class Solution(object):
-    def resultArray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        n=len(nums)
-        arr1=[]
-        arr2=[]
-        arr1.append(nums[0])
-        arr2.append(nums[1])
-        for i in range(2,n):
-            if arr1[len(arr1)-1]> arr2[len(arr2)-1]:
+class Solution:
+    def resultArray(self, nums: List[int]) -> List[int]:
+            n=len(nums)
+            arr1=[]
+            arr2=[]
+            arr1.append(nums[0])
+            arr2.append(nums[1])
+            for i in range(2,n):
+              if arr1[len(arr1)-1]> arr2[len(arr2)-1]:
                 arr1.append(nums[i])
-            else:
+              else:
                 arr2.append(nums[i])
         
         
-        return arr1+arr2
-    
+            return arr1+arr2
+
+        
         
