11cf6c6e07996c5be8233612e0d0839eeafebd51
IsmaBano <155432858+IsmaBano@users.noreply.github.com>
2026-08-24 20:04:38 +0530

    Time: 129 ms (5.11%) | Memory: 194.5 MB (5.11%) - LeetSync

---
+++ 2002-stone-game-viii/stone-game-viii.java
@@ -0,0 +1,44 @@
+class Solution {
+    Integer dp[][][];
+    public int solve (int idx,int a[],int ch,int t){
+        if(idx>=a.length){
+            if(t==0 && ch==0){
+                return Integer.MIN_VALUE;
+            }else if(t==0 && ch==1){
+                return Integer.MAX_VALUE;
+            }
+            return 0;
+        }
+        if(dp[idx][ch][t]!=null){
+            return dp[idx][ch][t];
+        }
+        //take
+       int take=solve(idx+1,a,1-ch,1);
+       if(ch==0 && take!=Integer.MAX_VALUE){
+        take=take+a[idx];
+       }else if(ch==1 && take!=Integer.MIN_VALUE){
+        take=take-a[idx];
+       }
+        //not take
+        if(idx==a.length-1){
+            return dp[idx][ch][t]=take;
+        }
+        int nt=solve(idx+1,a,ch,t);
+        if(ch==0){
+         return dp[idx][ch][t]= Math.max(take,nt);
+        }else{
+           return dp[idx][ch][t]=Math.min(take,nt);
+        }
+    }
+    public int stoneGameVIII(int[] stones) {
+        int n=stones.length;
+        int arr[]=new int[n];
+        dp=new Integer[n][2][2];
+        arr[0]=stones[0];
+        for(int i=1;i<n;i++){
+            arr[i]=stones[i]+arr[i-1];
+        }
+        int ans=solve(1,arr,0,0);
+        return ans;
+    }
+}
